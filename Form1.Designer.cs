35ecbaed33da7ef8e3c7c5939cc0e0b045f90453
its"Kchwasaunt <116268462+kaiaWANTSTOBEATUTUTBI@users.noreply.github.com>
2022-10-28 13:34:50 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Basic_Threading
+{
+    partial class FrmBasicThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblYAY = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblYAY
+            // 
+            this.lblYAY.AutoSize = true;
+            this.lblYAY.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYAY.Location = new System.Drawing.Point(27, 31);
+            this.lblYAY.Name = "lblYAY";
+            this.lblYAY.Size = new System.Drawing.Size(354, 37);
+            this.lblYAY.TabIndex = 0;
+            this.lblYAY.Text = " -Before starting thread-";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(150, 117);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(104, 39);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "rUN";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // FrmBasicThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 213);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblYAY);
+            this.Name = "FrmBasicThread";
+            this.Text = "BasicThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblYAY;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Basic_Threading
+{
+    public partial class FrmBasicThread : Form
+    {
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        public void Threads ()
+        {
+            lblYAY.Text = ("Before Starting Thread");
+            Console.WriteLine("-Before Starting Thread-");
+            
+            MyThreadClass obj = new MyThreadClass(); 
+            Thread ThreadA = new Thread(new ThreadStart(obj.Thread1));
+            Thread ThreadB = new Thread(new ThreadStart(obj.Thread1));
+            
+
+            ThreadA.Priority = ThreadPriority.Highest;
+            ThreadA.Name = "Thread A Process";
+            ThreadB.Priority = ThreadPriority.Highest;
+            ThreadB.Name = "Thread B Process";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            Console.WriteLine("-End of Thread-");
+            lblYAY.Text = ("-End of Thread-");
+
+
+
+        }
+
+        
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Threads();
+        }
+    }
+}
